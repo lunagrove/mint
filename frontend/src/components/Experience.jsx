efa016b9835bf5aebfec8e8ddb6aa3e0d3019858
lunagrove <sholmes47@my.bcit.ca>
2023-12-15 22:29:54 -0800

    Prepare for experience tagging

--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 import { IoTrashOutline } from "react-icons/io5";
 import { BsPencil } from "react-icons/bs";
 import { VscDebugBreakpointLog } from "react-icons/vsc";
 import { formatLongDate } from "../utilities/dates";
 
 const Experience = ({ snippet, onDelete }) => {
+
+    const [tags, setTags] = useState([]);
+
+    useEffect(() => {
+        let experienceTags = [];
+        if (snippet.roleIds) {
+            experienceTags.push(snippet.roleIds);
+        }
+        if (snippet.courseIds) {
+            experienceTags.push(snippet.courseIds);
+        }
+        if (snippet.credentialIds) {
+            experienceTags.push(snippet.credentialIds);
+        }
+        if (snippet.hobbyIds) {
+            experienceTags.push(snippet.hobbyIds);
+        }
+        if (snippet.projectIds) {
+            experienceTags.push(snippet.projectIds);
+        }
+        setTags(experienceTags);
+    }, []);
 
     const handleDeleteClick = () => {
         onDelete(snippet.experienceid);
@@ -21,6 +44,20 @@ const Experience = ({ snippet, onDelete }) => {
                 <div className="experience-details">
                     <h3 className="experience-snippet" >{snippet.snippet}</h3>
                     <h5 className="experience-date">Created on: {formatLongDate(snippet.createdon, false)}</h5>
+                    <div className="experience-skills">
+                        {snippet.skillIds && snippet.skillIds.length > 0 && snippet.skillIds.map((skill, index) => (
+                            <div key={index} className="skill-rectangle">
+                                {skill.description}
+                            </div>
+                        ))}
+                    </div>
+                    <div className="experience-tags">
+                        {tags && tags.length > 0 && tags.map((tag, index) => (
+                            <div key={index} className="skill-rectangle">
+                                {tag}
+                            </div>
+                        ))}
+                    </div>
                 </div>
                 <div className="experience-icons">
                     <BsPencil className="icon-medium edit-icon" onClick={handleEditClick}/>
